96dcb1dd25c58d8564bac43ab16468cf1019dc74
Welander1994 <64834767+Welander1994@users.noreply.github.com>
2020-11-27 09:35:58 +0100

    2.

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,27 @@
 console.log("javascript is running !");
 
-document.body.innerHTML = "<h1> hej verden fra en ekstern fil </h1>";
+let infoHeadline = ["1. Overskrift", "2. Overskrift", "3. Overskrift"];
+let infoImg = ["/img/img-1.jpg", "/img/img-2.jpg", "/img/img-3.jpg"];
+let infoText = ["text til 1. emne", "text til 2. emen", "text til 3. emne"];
 
-alert("davs")
+
+function changeText() {
+    document.getElementById("info-headline").innerHTML = infoHeadline[0];
+    document.getElementById("info-text").innerHTML = infoText[0];
+    document.getElementById("info-img").src = infoImg[0];
+
+  }
+
+  function changeText2() {
+    document.getElementById("info-headline").innerHTML = infoHeadline[1];
+    document.getElementById("info-text").innerHTML = infoText[1];
+    document.getElementById("info-img").src = infoImg[1];
+
+  }
+
+  function changeText3() {
+    document.getElementById("info-headline").innerHTML = infoHeadline[2];
+    document.getElementById("info-text").innerHTML = infoText[2];
+    document.getElementById("info-img").src = infoImg[2];
+
+  }
