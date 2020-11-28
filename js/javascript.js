220dd23c5868d8665fab9fa27e1357aed3ad28d2
Welander1994 <64834767+Welander1994@users.noreply.github.com>
2020-11-28 16:27:43 +0100

    kommentar
    skrevet kommentar til mit kode

--- js/javascript.js
+++ js/javascript.js
@@ -1,14 +1,26 @@
+/*tjekker om javascriptet kører*/
 console.log("javascript is running !");
 
+
+/*
+two dimensional array som indholder at det jeg gerne vil ændre på forside
+i et array - stillet op at hver punkt indholder infomationer som svare til sit punkt
+*/
 let forsideIndhold = [
   ["1. Overskrift","/img/img-1.jpg","text til 1. emne",],
   ["2. Overskrift","/img/img-2.jpg","text til 2. emne",],
   ["3. Overskrift","/img/img-3.jpg","text til 3. emne",],
 ];
 
+/* functionen under gøre ind og ændre indholdet på forsiden ud fra mit array forsideIndhold*/
 function changeFrontPage(i) {
   document.getElementById("info-headline").innerHTML = forsideIndhold[i][0];
   document.getElementById("info-img").src = forsideIndhold[i][1];
   document.getElementById("info-text").innerHTML = forsideIndhold[i][2];
 
 }
+/*
+[i][0] - her vælger du din overskrift
+[i][1] - her vælger du dit billede
+[i][2] - her vælger du din brødtekst
+*/
