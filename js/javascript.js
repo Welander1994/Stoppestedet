0ef9d76ca05e0369c63a3ed56d603a93ea4e49b7
Welander1994 <64834767+Welander1994@users.noreply.github.com>
2020-11-30 13:22:33 +0100

    fået min kode til at skrifte img ud med video

--- js/javascript.js
+++ js/javascript.js
@@ -10,6 +10,7 @@ let forsideIndhold = [
   ["1. Overskrift","/img/img-1.jpg","text til 1. emne",],
   ["2. Overskrift","/img/img-2.jpg","text til 2. emne",],
   ["3. Overskrift","/img/img-3.jpg","text til 3. emne",],
+  ["4. Overskrift"," ","text til 4. emne",],
 ];
 
 /* functionen under gøre ind og ændre indholdet på forsiden ud fra mit array forsideIndhold*/
@@ -18,7 +19,19 @@ function changeFrontPage(i) {
   document.getElementById("info-img").src = forsideIndhold[i][1];
   document.getElementById("info-text").innerHTML = forsideIndhold[i][2];
 
+  /*hvis man skal display en video istedet for et bilede så går man ind og 
+  skriver nummeret fra changeFontPage(i), så den ved hvor den skal vise video i stedet for billede*/
+  if (i == 3) {
+    document.getElementById("video").style.display = "block";
+    document.getElementById("info-img").style.display = "none";
+  } else {
+    document.getElementById("video").style.display = "none";
+    document.getElementById("info-img").style.display = "block";
+  }
+
 }
+
+
 /*
 [i][0] - her vælger du din overskrift
 [i][1] - her vælger du dit billede
