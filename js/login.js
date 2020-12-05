5962f1952c2a77ff2c0f110ac226aaf5368d7fb2
bene2510 <bene2510@edu.ucl.dk>
2020-12-05 15:49:44 +0100

    Login har nu både fået lidt css- og js-hejs
    Muhahahahaaaaaaaa - det virker sgu ;)

---
+++ js/login.js
@@ -0,0 +1,31 @@
+let myInput = document.getElementById("kode");
+let tal = document.getElementById("tal");
+let karakterer = document.getElementById("karakterer");
+
+myInput.onfocus = function() {
+    document.getElementById("besked").style.display = "block";
+}
+/* Gør at besked-boksen bliver vist, når brugeren trykker på kode-feltet*/
+myInput.onblur = function() {
+  document.getElementById("besked").style.display = "none";
+}
+
+/* Gør at beskedfelterne blive grønne, når man har opfyldt kravene til kodeordet */
+myInput.onkeyup = function() {
+    let taltal = /[0-9]/g;
+  if(myInput.value.match(taltal)) {  
+    tal.classList.remove("invalid");
+    tal.classList.add("valid");
+  } else {
+    tal.classList.remove("valid");
+    tal.classList.add("invalid");
+  }
+
+  if(myInput.value.length >= 8) {
+    karakterer.classList.remove("invalid");
+    karakterer.classList.add("valid");
+  } else {
+    karakterer.classList.remove("valid");
+    karakterer.classList.add("invalid");
+  }
+}
