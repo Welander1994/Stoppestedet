9c8c2355364f4a58b5ec58f9556cc3994d595c8f
Jacob Welander Jensen <64834767+Welander1994@users.noreply.github.com>
2020-12-07 12:51:23 +0100

    wip knapper på nogle af undersiderne

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,4 @@
-/*tjekker om javascriptet kører*/
+/* tjekker om javascriptet kører*/
 console.log("javascript is running !");
 
 function burger() {
@@ -6,10 +6,8 @@ function burger() {
 element.classList.toggle("main-nav-active");
 }
 
-/*
-two dimensional array som indholder at det jeg gerne vil ændre på forside
-i et array - stillet op at hver punkt indholder infomationer som svare til sit punkt
-*/
+/* two dimensional array som indholder at det jeg gerne vil ændre på forside
+   i et array - stillet op at hver punkt indholder infomationer som svare til sit punkt */
 let forsideIndhold = [
   ["Vi er her for at hjælpe dig","/img/forside.png","Stoppestedet er et SIND værested, der har eksisteret siden først i 90’erne. <b>Hos os er alle velkomne.</b> Stoppestedet er et anonymt værested, hvilket betyder, at tilbuddene kan benyttes frivilligt og uden visitering.<br><br> <b>Tak fordi du er her.</b> Hvis du vil vide mere om, hvem vi er, hvordan vi kan hjælpe dig og, hvordan du endda kan hjælpe os, er du velkommen til at læse mere om vores forskellige muligheder for lige netop dig.",],
   ["Om os",   "/img/img-2.jpg",   "Stoppestedet er et tilbud til borgere i Odense kommune om at kunne være i et åbent, anonymt mødested, hvor man møder støtte og forståelse med henblik på en bedre trivsel i hverdagen. <br> <br> Stoppestedet er en selvejende institution under landsforeningen SIND, hvor driften finansieres af Odense Kommune i henhold til indgået driftsoverenskomst. Stedet drives af en bestyrelse med repræsentanter fra SIND, medarbejdere, brugere og frivillige af Stoppestedet samt Odense Byråd. <br> <br> SIND arbejder for at fremme fysisk og psykisk sundhed, fordi vi mener, at psykiatriske og sociale indsatser skal indgå i en enhed, og fordi vi tror på, at det er muligt at komme sig af en sindslidelse.  <br> <br> SIND Værestedet Stoppestedet er et anonymt værested for borgere, der har en sindslidelse eller andre psykosociale problemstillinger. Stoppestedet er en café, der har åbent alle dage i ugen, hvor man hver dag kan komme og købe kaffe eller lidt at spise. Foruden lidt til den lille sult kan man også købe et varmt måltid mad seks dage i ugen, der laves i fællesskab mellem ansatte, frivillige og besøgende. Vi spiser kl. 17.30. Tilmelding til aftensmaden sker ved fremmøde eller telefonisk før kl. 16.00.  <br> <br> Meningen med Stoppestedet er først og fremmest at have et sted, hvor sindslidende synes, det er rart at være, og derfor bliver det sociale samvær også vægtet højt i caféen. Der afholdes løbende forskellige aktiviteter, man kan deltage i, hvis man har lyst. Vi følger årets traditioner og holder løbende fester – vi fejrer blandt andet juleaften på Stoppestedet.  <br> <br> Bestyrelsen Bestyrelsen består af Formand Brian Skov Nielsen, en medarbejderrepræsentant, en SIND-repræsentant, et medlem udpeget af Byrådet, en frivilligrepræsentant og to brugerrepræsentanter. Den daglige leder er sekretær.  <br> <br> Personale Til daglig er Stoppestedet styret af leder Birgit Boding, fem medarbejdere, der er tilknyttet caféen, en hel masse frivillige, et antal timelønnede konsulenter, der yder telefonrådgivning, én deltids sekretær, én deltids koordinator, der hjælper som bisidder, én medarbejder, som arbejder med udvikling af frivillighed i Stoppestedet samt Odense Kommunes socialpsykiatrisk tilbud.",],
@@ -27,39 +25,43 @@ let forsideIndhold = [
 
 ];
 
+/* array til link knapper */
 let linkDestination = [
-  ["link til siden1", "navn på link1",],
-  ["link til siden2", "navn på link2",],
-  ["link til siden3", "navn på link3",],
-  ["link til siden4", "navn på link4",],
-  ["link til siden5", "navn på link5",],
-  ["link til siden6", "navn på link6",],
-  ["link til siden6", "navn på link7",],
-  ["link til siden7", "navn på link8",],
-  ["link til siden8", "navn på link9",],
-  ["link til siden9", "navn på link10",],
-  ["link til siden11", "navn på link11",],
-  ["link til siden12", "navn på link12",],
-  ["link til siden13", "navn på link13",],
+  [" ", " ",],
+  [" ", " ",],
+  [" ", " ",],
+  [" ", " ",],
+  ["link til siden5", "Læs mere her",],
+  ["link til siden6", "Læs mere her ",],
+  ["link til siden6", "Læs mere her","Find os på Goolge Maps"],
+  ["link til siden7", "Læs mere her ",],
+  ["link til siden8", "navn på link",],
+  ["link til siden9", "Ansøg her ",],
+  ["link til siden11", "Find os på Goolge Maps",],
+  [" ", " ",],
+  [" ", " ",],
 
 ]
-
+ 
 
 
 /* functionen under gøre ind og ændre indholdet på forsiden ud fra mit array forsideIndhold*/
 function changeFrontPage(i) {
   document.getElementById("info-headline").innerHTML = forsideIndhold[i][0];
   document.getElementById("info-img").src = forsideIndhold[i][1];
-  document.getElementById("info-img").alt = forsideIndhold[i][1];
+  document.getElementById("info-img").alt = forsideIndhold[i][3];
   document.getElementById("info-text").innerHTML = forsideIndhold[i][2];
 
   /* ændre href og navn til links */
   document.getElementById("link").href = linkDestination[i][0];
   document.getElementById("link").innerHTML = linkDestination[i][1];
 
+  document.getElementById("link2").href = linkDestination[i][0];
+  document.getElementById("link2").innerHTML = linkDestination[i][2];
 
-  /*hvis man skal display en video istedet for et bilede så går man ind og 
-  skriver nummeret fra changeFontPage(i), så den ved hvor den skal vise video i stedet for billede*/
+
+  /* hvis man skal display en video istedet for et bilede så går man ind og 
+     skriver nummeret fra changeFontPage(i), så den ved hvor den skal vise video i stedet for billede*/
   if (i == 3) {
     document.getElementById("video").style.display = "block";
     document.getElementById("video2").style.display = "block";
@@ -71,11 +73,20 @@ function changeFrontPage(i) {
   }
 
 
-  if (i == 4,5) {
+  /* kontroller hvor knapperne skal vise*/
+  if (i == 4 || i == 5 || i == 6 || i == 7 || i == 9 || i == 10)  {
     document.getElementById("link").style.display = "block";
+    document.getElementById("link2").style.display = "block";
   }
   else {
     document.getElementById("link").style.display = "none";
+  }
+  /* viser goolge knappen på skibhus kabyssen*/
+  if ( i == 6)  {
+    document.getElementById("link2").style.display = "block";
+  }
+  else {
+    document.getElementById("link2").style.display = "none";
   }
 
 
