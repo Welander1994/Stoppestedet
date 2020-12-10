61fae0652d2b06465e764508e64ec8162c4ed9cf
idax6797 <idax6797@edu.ucl.dk>
2020-12-10 20:21:56 +0100

    Mere styleguide - grid er ikke for godt?

---
+++ js/styleguide.js
@@ -0,0 +1,33 @@
+function introFunction() {
+    var elmnt = document.getElementById("intro");
+    elmnt.scrollIntoView();
+}
+
+function tovFunction() {
+    var elmnt = document.getElementById("tov");
+    elmnt.scrollIntoView();
+}
+
+function logoFunction() {
+    var elmnt = document.getElementById("logo");
+    elmnt.scrollIntoView();
+}
+function farverFunction() {
+    var elmnt = document.getElementById("farver");
+    elmnt.scrollIntoView();
+}
+
+function typoFunction() {
+    var elmnt = document.getElementById("typo");
+    elmnt.scrollIntoView();
+}
+function gfFunction() {
+    var elmnt = document.getElementById("gf");
+    elmnt.scrollIntoView();
+}
+function gridFunction() {
+    var elmnt = document.getElementById("grid");
+    elmnt.scrollIntoView();
+}
+
+
